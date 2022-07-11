249c0b5991562a092a9b073d7cef8f3530fc2ee2
TT <46784262+TT-World@users.noreply.github.com>
2022-07-11 12:14:49 +0200

    Second Changes

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ProMovies
@@ -12,6 +8,13 @@ namespace ProMovies
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"2015|2016", month = @"\d{2}" }
+                );
 
             routes.MapRoute(
                 name: "Default",
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using ProMovies.Models;
+using System.Web.Mvc;
+
+namespace ProMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // this will be called when we go here  GET: Movies/Random
+        public ActionResult Random()
+        {
+            //create intance of our movie model
+            var movie = new Movie() { Name = "Shrek! TT" };
+            return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        //Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + " / " + month);
+        }
+
+    }
+}
