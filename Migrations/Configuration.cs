1cf12729141fc48bcf037e3e97d98dba0082bd79
TT <46784262+TT-World@users.noreply.github.com>
2022-07-11 17:45:26 +0200

    last commit for the day

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ProMovies.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProMovies.Models.myData.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProMovies.Models.myData.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Models/myData.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+
+namespace ProMovies.Models
+{
+    public class myData
+    {
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext()
+            {
+            }
+        }
+    }
+}
